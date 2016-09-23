6af969c9825e0f50dd8459fee93ba4f7ed92f3ce
cody.b.gordon@gmail.com <cody.b.gordon@gmail.com>
2016-09-23 08:48:25 -0700

    new

---
+++ app.js
@@ -0,0 +1,29 @@
+//Do Not Modify the getMusic function
+function getMusic() {
+    var artist = document.getElementById('artist').value;
+    itunes.getMusicByArtist(artist).then(drawSongs);
+}
+
+
+
+
+function drawSongs(songList) {
+    console.log(songList);
+
+    var container = $('$music-container')
+    container.html('')
+    songList.forEach(function(song) {
+
+        var card = `
+    <div>
+    <h3>${song.title}</h3>
+    <img src='${song.albumArt}'></img>
+    </div>`
+
+container.append(card)
+
+    })
+}
+
+
+
